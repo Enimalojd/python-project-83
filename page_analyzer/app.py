9b40b6cd49f9b761bd4cc33edff614ba8c730d41
enimalojd <enderston8@gmail.com>
2024-02-03 00:00:19 +0400

    fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-import config
+from config import SECRET_KEY
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route('/')

30a5c2adeaa2d4510f7f70ad9c86902ddea16b64
enimalojd <enderston8@gmail.com>
2024-02-08 02:13:29 +0400

    change post urls handler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 from config import SECRET_KEY, DATABASE_URL
@@ -8,7 +8,6 @@ from .validate import validator
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-
 
 
 @app.route('/')
@@ -26,6 +25,7 @@ def get_urls():
             with conn.cursor() as curs:
                 curs.execute("SELECT id, name FROM urls ORDER BY created_at ASC;")
                 urls = curs.fetchall()
+    # СЮДА НАПИСАТЬ ОБРАБОТЧИК ОШИБОК ПОДКЛЮЧЕНИЯ К БАЗЕ
     finally:
         conn.close()
     return render_template('urls.html', messages=messages, urls=urls)
@@ -42,19 +42,19 @@ def post_urls():
         conn = psycopg2.connect(DATABASE_URL)
         with conn:
             with conn.cursor() as curs:
-                print('Подключение установлено')
-                curs.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;", (url, datetime.now()))
-                inser_id = curs.fetchone()[0]
-                flash('Сайт успешно добавлен')
-                return redirect(url_for('get_url', id=inser_id), code=302)
-    except psycopg2.IntegrityError:
-        inser_id = curs.fetchone()[0]
-        conn.rollback()
-        flash('Такой сайт уже существует')
-        return redirect(url_for('get_url', id=inser_id), code=302)
-    finally:
-        if conn:
-            conn.close()
+                curs.execute("SELECT id FROM urls WHERE name = (%s);", (url,))
+                url_id = curs.fetchone()
+                if url_id:
+                    flash('Такой сайт уже существует!')
+                    return redirect(url_for('get_url', id=url_id[0]), code=302)
+                else:
+                    curs.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;", (url, date.today()))
+                    url_id = curs.fetchone()
+                    flash('Сайт успешно добавлен!')
+                    return redirect(url_for('get_url', id=url_id[0]), code=302)
+    except Exception as e:
+        flash('Что-то пошло не так!')
+        return redirect('/')
 
 
 @app.get('/urls/<id>')

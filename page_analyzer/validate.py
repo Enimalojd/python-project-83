f8e68650c9d0d01d727441923602897c6fe2126d
enimalojd <enderston8@gmail.com>
2024-02-24 01:59:08 +0400

    change validator

--- page_analyzer/validate.py
+++ page_analyzer/validate.py
@@ -1,5 +1,21 @@
+import re
+
+
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+
 def validator(url):
     errors = {}
     if len(url) > 255:
-        errors['url'] = 'URL is too long'
+        errors['url'] = 'URL первышает 255 символов'
+    if (re.match(regex, url) is not None) is False:
+        errors['url'] = 'Некорректный URL'
+    if url[-1] == '.':
+        errors['url'] = 'Некорректный URL'
     return errors

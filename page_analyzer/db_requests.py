5973fd4c5cbbda9431eeb765149723183dd83009
enimalojd <enderston8@gmail.com>
2024-02-24 01:59:52 +0400

    fix

--- page_analyzer/db_requests.py
+++ page_analyzer/db_requests.py
@@ -1,4 +1,4 @@
-SELECT_ALL_URLS = "SELECT id, name FROM urls ORDER BY created_at ASC;"
+SELECT_ALL_URLS = "SELECT id, name FROM urls ORDER BY id DESC;"
 CHECK_FOR_MATCHES = "SELECT id FROM urls WHERE name = (%s);"
 ADD_URL = "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;"
 SELECT_DATA = "SELECT id, name, created_at FROM urls WHERE id = (%s);"

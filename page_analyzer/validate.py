19ac0ca5cc7950dfbc136e7089ba7385311ad399
enimalojd <enderston8@gmail.com>
2024-02-26 21:23:53 +0400

    fix

--- page_analyzer/validate.py
+++ page_analyzer/validate.py
@@ -1,13 +1,12 @@
 import re
 
 
-regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+regex = re.compile(r'^(?:http|ftp)s?://'
+                   r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # noqa
+                   r'localhost|'
+                   r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+                   r'(?::\d+)?'
+                   r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
 def validator(url):

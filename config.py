00eadba6369d3460d0b2b872c8d380996585a439
enimalojd <enderston8@gmail.com>
2024-03-18 23:08:14 +0300

    change config

--- config.py
+++ config.py
@@ -5,8 +5,3 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_PASS = os.getenv("DATABASE_PASS")
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_HOST = os.getenv("DATABASE_HOST")
-DATABASE_PORT = os.getenv("DATABASE_PORT")

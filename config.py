727faf7f2ee3afa9903aeff15b8a224ea2bc695a
enimalojd <enderston8@gmail.com>
2024-03-18 22:57:38 +0300

    fix

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 DATABASE_PASS = os.getenv("DATABASE_PASS")
 DATABASE_USER = os.getenv("DATABASE_USER")

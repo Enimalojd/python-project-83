244b461086f4fef629809edb1698201843f4eaef
enimalojd <enderston8@gmail.com>
2024-03-18 23:52:46 +0300

    fix putconn

--- page_analyzer/handlers.py
+++ page_analyzer/handlers.py
@@ -3,8 +3,7 @@ from .db import connection
 
 
 def get_all_urls():
-    conn = connection.getconn()
-    with conn:
+    with connection.getconn() as conn:
         with conn.cursor() as curs:
             curs.execute("""
                     SELECT u.id, u.name, uc.created_at, uc.status_code
@@ -20,33 +19,27 @@ def get_all_urls():
                     ) uc ON u.id = uc.url_id
                     ORDER BY u.id DESC;
                 """)
-            connection.putconn(conn)
             return curs.fetchall()
 
 
 def check_url_existence(url):
-    conn = connection.getconn()
-    with conn:
+    with connection.getconn() as conn:
         with conn.cursor() as curs:
             curs.execute("""SELECT id FROM urls WHERE name = (%s);""", (url,))
-            connection.putconn(conn)
             return curs.fetchone()
 
 
 def add_new_url(url):
-    conn = connection.getconn()
-    with conn:
+    with connection.getconn() as conn:
         with conn.cursor() as curs:
             curs.execute("""INSERT INTO urls (name, created_at)
                          VALUES (%s, %s) RETURNING id;""",
                          (url, date.today()))
-            connection.putconn(conn)
             return curs.fetchone()
 
 
 def get_url_data(url_id):
-    conn = connection.getconn()
-    with conn:
+    with connection.getconn() as conn:
         with conn.cursor() as curs:
             curs.execute("""SELECT id, name, created_at
                          FROM urls WHERE id = (%s);""",
@@ -58,13 +51,11 @@ def get_url_data(url_id):
                          (url[0][0],))
             checks = curs.fetchall()
             data = (url, checks)
-            connection.putconn(conn)
             return data
 
 
 def get_url_name_by_id(url_id):
-    conn = connection.getconn()
-    with conn:
+    with connection.getconn() as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT name FROM urls WHERE id = %s;", (int(url_id),))
             connection.putconn(conn)
@@ -72,8 +63,7 @@ def get_url_name_by_id(url_id):
 
 
 def insert_new_check(url_id, data):
-    conn = connection.getconn()
-    with conn:
+    with connection.getconn() as conn:
         with conn.cursor() as cur:
             cur.execute("""
                 INSERT INTO url_checks (url_id, created_at,

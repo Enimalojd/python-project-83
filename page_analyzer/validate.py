0923ebd37167a415311865928923f5dc508d6892
enimalojd <enderston8@gmail.com>
2024-03-23 18:44:23 +0300

    add dep validators and change validation logic

--- page_analyzer/validate.py
+++ page_analyzer/validate.py
@@ -1,23 +1,10 @@
-import re
+import validators
 
 
-regex = re.compile(r'^(?:http|ftp)s?://'
-                   r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # noqa
-                   r'localhost|'
-                   r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-                   r'(?::\d+)?'
-                   r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-
-def validator(url):
+def valid_url(url):
     errors = {}
-    if url:
-        if len(url) > 255:
-            errors['url'] = 'URL первышает 255 символов'
-        if (re.match(regex, url) is not None) is False:
-            errors['url'] = 'Некорректный URL'
-        if url[-1] == '.':
-            errors['url'] = 'Некорректный URL'
-        return errors
-    errors['url'] = 'Некорректный URL'
+    if not validators.url(url):
+        errors['url'] = 'Некорректный URL'
+    if len(url) > 255:
+        errors['url'] = 'URL первышает 255 символов'
     return errors

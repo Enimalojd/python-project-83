fa51b551b82bea6eef6ebe8dc4c12cfd7e0b0ed8
enimalojd <enderston8@gmail.com>
2024-03-23 18:45:39 +0300

    add new func logic for create connection pool

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,9 +1,26 @@
+from contextlib import contextmanager
 from psycopg2 import pool
 from config import DATABASE_URL
 
 
-connection = pool.SimpleConnectionPool(
-    1,
-    10,
-    DATABASE_URL
-)
+def create_connection_pool():
+    connection_pool = pool.SimpleConnectionPool(1,
+                                                10,
+                                                DATABASE_URL
+                                                )
+    return connection_pool
+
+
+@contextmanager
+def get_connection_pool():
+    global connect
+    if not connect:
+        connect = create_connection_pool()
+    try:
+        conn = connect.getconn()
+        yield conn
+    finally:
+        connect.putconn(conn)
+
+
+connect = create_connection_pool()
